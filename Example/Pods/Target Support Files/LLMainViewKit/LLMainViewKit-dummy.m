a35c637938ffa926fe06b66cde18918a7b4b2973
hulilei <huuuu1016@gmail.com>
2021-08-26 16:46:44 +0800

    add files

---
+++ Example/Pods/Target Support Files/LLMainViewKit/LLMainViewKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LLMainViewKit : NSObject
+@end
+@implementation PodsDummy_LLMainViewKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-LLMainViewKit_Tests/Pods-LLMainViewKit_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LLMainViewKit_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LLMainViewKit_Tests
+@end
